0090616232fb2d8ebfe4b39644604aa543478e03
annadokuchaeva2002 <101467150+annadokuchaeva2002@users.noreply.github.com>
2023-01-12 21:19:45 +0300

    Add files via upload

---
+++ wands.js
@@ -0,0 +1,55 @@
+document.getElementById('pageWands').onclick = function() {
+    window.location.href = 'pageWands.html';
+};
+
+document.getElementById('knightWands').onclick = function() {
+    window.location.href = 'knightWands.html';
+};
+
+document.getElementById('queenWands').onclick = function() {
+    window.location.href = 'queenWands.html';
+};
+
+document.getElementById('kingWands').onclick = function() {
+    window.location.href = 'kingWands.html';
+};
+
+document.getElementById('1wands').onclick = function() {
+    window.location.href = '1wands.html';
+};
+
+document.getElementById('2wands').onclick = function() {
+    window.location.href = '2wands.html';
+};
+
+document.getElementById('3wands').onclick = function() {
+    window.location.href = '3wands.html';
+};
+
+document.getElementById('4wands').onclick = function() {
+    window.location.href = '4wands.html';
+};
+
+document.getElementById('5wands').onclick = function() {
+    window.location.href = '5wands.html';
+};
+
+document.getElementById('6wands').onclick = function() {
+    window.location.href = '6wands.html';
+};
+
+document.getElementById('7wands').onclick = function() {
+    window.location.href = '7wands.html';
+};
+
+document.getElementById('8wands').onclick = function() {
+    window.location.href = '8wands.html';
+};
+
+document.getElementById('9wands').onclick = function() {
+    window.location.href = '9wands.html';
+};
+
+document.getElementById('10wands').onclick = function() {
+    window.location.href = '10wands.html';
+};
