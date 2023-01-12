1689d30a93ddb191db08d5dd080c3a892e971915
annadokuchaeva2002 <101467150+annadokuchaeva2002@users.noreply.github.com>
2023-01-12 21:19:03 +0300

    Add files via upload

---
+++ pentacles.js
@@ -0,0 +1,56 @@
+document.getElementById('pagePentacles').onclick = function() {
+    window.location.href = 'pagePentacles.html';
+};
+
+document.getElementById('knightPentacles').onclick = function() {
+    window.location.href = 'knightPentacles.html';
+};
+
+document.getElementById('queenPentacles').onclick = function() {
+    window.location.href = 'queenPentacles.html';
+};
+
+document.getElementById('kingPentacles').onclick = function() {
+    window.location.href = 'kingPentacles.html';
+};
+
+document.getElementById('1Pentacles').onclick = function() {
+    window.location.href = '1pentacles.html';
+};
+
+document.getElementById('2Pentacles').onclick = function() {
+    window.location.href = '2pentacles.html';
+};
+
+document.getElementById('3Pentacles').onclick = function() {
+    window.location.href = '3pentacles.html';
+};
+
+document.getElementById('4Pentacles').onclick = function() {
+    window.location.href = '4pentacles.html';
+};
+
+document.getElementById('5Pentacles').onclick = function() {
+    window.location.href = '5pentacles.html';
+};
+
+document.getElementById('6Pentacles').onclick = function() {
+    window.location.href = '6pentacles.html';
+};
+
+document.getElementById('7Pentacles').onclick = function() {
+    window.location.href = '7pentacles.html';
+};
+
+document.getElementById('8Pentacles').onclick = function() {
+    window.location.href = '8pentacles.html';
+};
+
+document.getElementById('9Pentacles').onclick = function() {
+    window.location.href = '9pentacles.html';
+};
+
+document.getElementById('10Pentacles').onclick = function() {
+    window.location.href = '10pentacles.html';
+};
+
---
+++ swords.js
@@ -0,0 +1,55 @@
+document.getElementById('pageSwords').onclick = function() {
+    window.location.href = 'pageSwords.html';
+};
+
+document.getElementById('knightSwords').onclick = function() {
+    window.location.href = 'knightSwords.html';
+};
+
+document.getElementById('queenSwords').onclick = function() {
+    window.location.href = 'queenSwords.html';
+};
+
+document.getElementById('kingSwords').onclick = function() {
+    window.location.href = 'kingSwords.html';
+};
+
+document.getElementById('1Swords').onclick = function() {
+    window.location.href = '1swords.html';
+};
+
+document.getElementById('2Swords').onclick = function() {
+    window.location.href = '2swords.html';
+};
+
+document.getElementById('3Swords').onclick = function() {
+    window.location.href = '3swords.html';
+};
+
+document.getElementById('4Swords').onclick = function() {
+    window.location.href = '4swords.html';
+};
+
+document.getElementById('5Swords').onclick = function() {
+    window.location.href = '5swords.html';
+};
+
+document.getElementById('6Swords').onclick = function() {
+    window.location.href = '6swords.html';
+};
+
+document.getElementById('7Swords').onclick = function() {
+    window.location.href = '7swords.html';
+};
+
+document.getElementById('8Swords').onclick = function() {
+    window.location.href = '8swords.html';
+};
+
+document.getElementById('9Swords').onclick = function() {
+    window.location.href = '9swords.html';
+};
+
+document.getElementById('10Swords').onclick = function() {
+    window.location.href = '10swords.html';
+};
